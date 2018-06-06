09bb7ee199c9a0d7c7c5b14142a82bd4caa653f9
babis <theodoroucharalabos@gmail.com>
2018-06-06 20:31:26 +0300

    working with react

--- adding/src/App.js
+++ adding/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NumericInput from 'react-numeric-input';
+NumericInput.style.input.color = 'white';
 
 class App extends Component {
   render() {
@@ -8,11 +10,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React and JDango</h1>
+          <h1 className="App-title">Welcome to React and Django</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <NumericInput  min={0} max={100} value={0}/>
+        <NumericInput  min={0} max={100} value={0}/>
+        <button type="button">Calculate</button>
       </div>
     );
   }
